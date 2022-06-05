15a50719509c40513591a4e5619109e3732d4f07
null-2020 <f1.siddiqui@yandex.com>
2022-06-04 22:27:37 -0400

    Added the tab icon, Header.js, and index.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,12 @@ import React from "react";
 export default function Header() {
   return (
     <header className="header">
-      <h1>Header</h1>
+      <div className="top-bar">
+        <img src="/images/Trollface.png" width="40px" />
+        <h1 class="title">Meme Generator</h1>
+        <img src="/images/Trollface.png" width="40px" />
+      </div>
+      <h2>Insert Paragraph Here</h2>
     </header>
   );
 }
