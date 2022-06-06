f6f493ec2b186603a785b3d067377304263ae02e
null-2020 <f1.siddiqui@yandex.com>
2022-06-06 19:33:02 -0400

    Added the contents for Meme.js + fixed header images

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
+import trollFace from "../images/Trollface.png";
 
 export default function Header() {
   return (
     <header className="header">
       <div className="top-bar">
-        <img src="/images/Trollface.png" width="40px" />
-        <h1 class="title">Meme Generator</h1>
-        <img src="/images/Trollface.png" width="40px" />
+        <img src={trollFace} width="60px" />
+        <h1 className="title">Meme Generator</h1>
+        <img src={trollFace} width="60px" />
       </div>
       <h2>Insert Paragraph Here</h2>
     </header>
